be5ad3bd9a988d22fb7132cb3898d72011087f27
Roberto Reale <roberto.reale@linux.com>
2017-01-09 17:10:17 +0100

    Changed author's email and package url.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name = 'wsync',
-    version = '0.0.8',
+    version = '0.0.8.1',
     description = 'A small Python utility for synchronizing a local folder '\
     'with a remote web repository',
     py_modules = [ 'wsync' ],
     author = 'Roberto Reale',
-    author_email = 'roberto.reale82@gmail.com',
-    url = 'https://github.com/roberto-reale/wsync',
+    author_email = 'roberto.reale@linux.com',
+    url = 'https://github.com/robertoreale/wsync',
     keywords = [ 'wsync', 'sync', 'http', 'https', 'web' ],
     install_requires = [ 
     'requests',
